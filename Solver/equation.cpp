573ad9557c41677030218ee3449649260aab9d13
Zane Urbanski <zane@Zanes-MacBook-Air.local>
2014-10-18 23:31:25 -0500

    added support for negative numbers

--- Solver/equation.cpp
+++ Solver/equation.cpp
@@ -3,14 +3,22 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
-void pop_all_ops(std::vector<double>& postfix, std::stack<char>& ops) {
+using namespace std;
+
+string pop_all_ops(vector<double>& postfix, stack<char>& ops) {
   char op;
   double num1, num2;
+  string steps = "";
+  char buffer[128];
 
+  // Pop all the operations off the ops stack, and push the result onto the
+  // postfix vector.
   while(!ops.empty()) {
     op = ops.top();
     ops.pop();
+
     num2 = postfix.back();
     postfix.pop_back();
     num1 = postfix.back();
@@ -18,34 +26,50 @@ void pop_all_ops(std::vector<double>& postfix, std::stack<char>& ops) {
 
     if(op == '+') {
       postfix.push_back(num1 + num2);
-    } else if(op == '-') {
+    }
+    else if(op == '-') {
       postfix.push_back(num1 - num2);
-    } else if(op == '*') {
+    }
+    else if(op == '*') {
       postfix.push_back(num1 * num2);
-    } else if(op == '/') {
+    }
+    else if(op == '/') {
       postfix.push_back(num1 / num2);
-    } else if(op == '^') {
+    }
+    else if(op == '^') {
       postfix.push_back(pow(num1, num2));
-    } else {
+    }
+    else {
       throw -1;
     }
+
+    // Add the step we just took to the steps string.
+    sprintf(buffer, "%g %c %g = %g\n", num1, op, num2, postfix.back());
+    steps += string(buffer);
   }
+
+  return steps;
 }
 
-double expression(std::string a) {
-  std::vector<double> postfix;
-  std::stack<char> ops;
+pair<double, string> expression(string a) {
+  vector<double> postfix;
+  stack<char> ops;
+  pair<double, string> temp_pair;
+  string result = "";
 
+  // Remove all whitespace from the input string.
+  a.erase(remove_if(a.begin(), a.end(), (int(*)(int))isspace), a.end());
   int parens;
 
   postfix.push_back(0);
 
   for(int i = 0; i < a.length(); ++i) {
-    if(a[i] == ' ') {
-      continue;
-    } else if(a[i] == '(') {
+    // If we encounter an open parentheses, move through the string until we
+    // find the matching closing parentheses. Recursively call expression()
+    // on the substring and add the result to the postfix vector.
+    if(a[i] == '(') {
       parens = 0;
-      std::string sub = "";
+      string sub = "";
       ++i;
 
       while(!(a[i] == ')' && parens == 0)) {
@@ -59,40 +83,64 @@ double expression(std::string a) {
         ++i;
       }
       
+      temp_pair = expression(sub);
+
       postfix.pop_back();
-      postfix.push_back(expression(sub));
-    } else if(a[i] >= '0' && a[i] <= '9') {
+      postfix.push_back(temp_pair.first);
+
+      result += temp_pair.second;
+    }
+    // New digit to add.
+    else if(a[i] >= '0' && a[i] <= '9') {
       postfix[postfix.size() - 1] *= 10;
       postfix[postfix.size() - 1] += a[i] - '0';
-    } else {
+    }
+    // If we encounter a negative sign and the character immediately preceding
+    // it is not a number, we know that this minus sign is intended identify
+    // the following number as negative.
+    else if((a[i] == '-' && i == 0) ||
+            (a[i] == '-' && i > 0 &&
+            (a[i - 1] <= '0' || a[i - 1] >= '9'))) {
+      ops.push('*');
+
+      postfix.pop_back();
+      postfix.push_back(-1);
+      postfix.push_back(0);
+    }
+    // If the operator we encounter has lower precedence than the operator
+    // on the top of the stack, we need pop all the operators off the stack
+    // and change the postfix vector to a single result.
+    else {
       if(((a[i] == '+' || a[i] == '-') && !ops.empty() &&
          (ops.top() == '*' || ops.top() == '/' || ops.top() == '^')) ||
          ((a[i] == '*' || a[i] == '/') && !ops.empty() &&
          ops.top() == '^')) {
-        pop_all_ops(postfix, ops);
+        result += pop_all_ops(postfix, ops);
       }
 
       ops.push(a[i]);
+
       postfix.push_back(0);
     }
   }
 
-  pop_all_ops(postfix, ops);
+  result += pop_all_ops(postfix, ops);
 
-  return postfix.back();
+  return make_pair(postfix.back(), result);
 }
 
 int main() {
   // make the big switch statement here
-  std::string input;
+  string input;
 
   while(true) {
-    getline(std::cin, input);
-    if(!std::cin) {
+    getline(cin, input);
+    if(!cin) {
       break;
     }
 
-    std::cout << expression(input) << std::endl;
+    cout << expression(input).second << endl;
+    cout << "Result: " << expression(input).first << endl << endl;
   }
 
   return 0;
